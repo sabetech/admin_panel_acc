fc4d25baab3b55c43e2bf31d16186243e2035397
Albert <albert.mensahansah@gmail.com>
2021-07-30 14:41:51 +0000

    current ssmg status

--- client/src/views/ssmg/constituency_ssmg.js
+++ client/src/views/ssmg/constituency_ssmg.js
@@ -23,6 +23,98 @@ export default function ConstituencySSMG() {
                         }, {
                             requestParam:'HearingAndSeeing',
                             readableName:'Hearing And Seeing'
+                        },{
+                            requestParam:'AntiBrutish',
+                            readableName:'AntiBrutish'
+                        },{
+                            requestParam:'CentersStarted',
+                            readableName:'Centers Started'
+                        },
+                        {
+                            requestParam:'BacentasStarted',
+                            readableName:'Bacentas Started'
+                        },{
+                            requestParam:'WeeklySaturdayAttendance',
+                            readableName:'Weekly Saturday Attendance (Bacenta Proliferation)'
+                        },{
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_greater_love',
+                            readableName:'Greater Love Gospel Choir (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_greater_love',
+                            readableName:'Greater Love Gospel Choir (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_dancing_stars',
+                            readableName:'Dancing Stars (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_dancing_stars',
+                            readableName:'Dancing Stars (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_film_stars',
+                            readableName:'Film Stars (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_film_stars',
+                            readableName:'Film Stars (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_airport_stars',
+                            readableName:'Airport Stars (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_airport_stars',
+                            readableName:'Airport Stars (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_ushers',
+                            readableName:'Ushers (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_ushers',
+                            readableName:'Ushers (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_telepastors',
+                            readableName:'Telepastors (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_telepastors',
+                            readableName:'Telepastors (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_arrivals',
+                            readableName:'Arrivals (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_arrivals',
+                            readableName:'Arrivals (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_communion',
+                            readableName:'Communion (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_communion',
+                            readableName:'Communion (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sat_attn_hearing_seeing',
+                            readableName:'Hearing and Seeing (Fri)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=sun_attn_hearing_seeing',
+                            readableName:'Hearing and Seeing (Sat)'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=no_people_counselled',
+                            readableName:'Number of people Counselled'
+                        },
+                        {
+                            requestParam:'BasontaAttendance&basonta_field=no_counsellors',
+                            readableName:'Number of Counsellors'
                         }
                     ];
 
